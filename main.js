4d9596925b8407a540315320c20050bdcba8ba05
Daniel Beebe <danwbeebe@gmail.com>
2017-07-12 12:00:24 -0400

    CSS style edits to header

---
+++ main.js
@@ -0,0 +1,37 @@
+// Phase One: Continent Selection
+
+  // Side narrative:
+
+  // Alternating turns
+  //   Random whether general or diplomat starts
+  //   Each player selects 3 continents
+
+  // The boxes under the continents:
+  //   Turn box: says whose turn it is
+  //   Status box: says which continent was last selected
+  //   World box: says that players are selecting continents throughout this phase
+
+// Phase Two: Game Play
+
+ // Alternating turns
+
+ //    For a move:
+ //        Player clicks on a continent belonging to the other player
+ //        Random determination whether the move is a win or a loss
+ //        If win, the player gains that continent
+ //        If loss, the player loses a random continent
+
+ //    The boxes below the continents:
+ //        Turn box: says whose turn it is
+ //        Status box: says which continent was last attacked and the result
+ //        World box: says that the world is in jeopardy throughout this phase
+
+// Phase Three: Selecting A Winner
+
+// If a player controls all 6 continents, game over.
+//     The surviving player wins
+//     World box:
+//         If general wins, says that the world is at war
+//         If diplomat wins, says the world is at peace
+
+// Resetting and playing again
