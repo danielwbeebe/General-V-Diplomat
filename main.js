2bb7cd78f35db420862202b2ca305d6a9d5c6281
Daniel Beebe <danwbeebe@gmail.com>
2017-07-14 11:05:09 -0400

    added variables to identify previous turn's player and whether move was successful

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Setting variable for player turn
 var playerTurn = "";
 
+// Setting variable for previous turn's player
+var otherTurn = "";
+
 // Creating empty arrays with continents for each player
 gArray = [];
 dArray = [];
@@ -14,7 +17,8 @@ function clickRules() {
   document.getElementById("announceText").innerHTML = "\'General Versus Diplomat\' is a 2-player game. To begin, each player chooses 3 continents. Next, the game play begins.<br><br>The General 'attacks' one of the Diplomat's continents for each turn. The Diplomat 'negotiates' with one of the General's continents for each turn. If a player is successful during a turn, that player takes control of the continent. But losing means the player loses a continent.<br><br>A player wins by controlling all 6 continents.";
 };
 
-  // Change aside section and 3 lower boxes in the continent selecting phase of the game
+/* Function starting game, changing board messages and calling random player function
+to choose to starts and function to add event listeners to continent boxes */
 function startGame() {
 
   // Calling function to set random player to start
@@ -26,7 +30,7 @@ function startGame() {
   // Changing text on game board for continent selection phase
   document.getElementById("announceTitle").innerHTML = "Begin Selecting";
   document.getElementById("announceText").innerHTML = "General and Diplomat, welcome.<br><br>You will alternate selecting the continents you wish to control.<br><br>You each get 3 continents.<br><br>Choose wisely.";
-  document.getElementById("status-text").innerHTML = "General and Diplomat, you will each need to pick 3 continents.";
+  document.getElementById("status-text").innerHTML = "Pick your continents.";
   document.getElementById("turn-message").innerHTML = `${playerTurn}, it's your turn.`;
 };
 
@@ -102,8 +106,8 @@ function selectDone() {
 function playGame() {
 
   document.getElementById("announceTitle").innerHTML = "Begin The Struggle";
-  document.getElementById("announceText").innerHTML = "General and Diplomat, the fight is now.<br><br>Whether the world suffers war or enjoys peace is up to you!<br><br>May the better player win!";
-  document.getElementById("status-text").innerHTML = "General and Diplomat, you will each need to fight it out until someone wins.";
+  document.getElementById("announceText").innerHTML = "General and Diplomat, the fight is now.<br><br>General, attack BLUE continents.<br><br>Diplomat, negotiate with RED continents.<br><br>Whether the world suffers war or enjoys peace is up to you!<br><br>May the better player win!";
+  document.getElementById("status-text").innerHTML = "General, attack BLUE continents.<br>Diplomat, negotiate with RED continents.";
   document.getElementById("turn-message").innerHTML = `${playerTurn}, it's your turn.`;
 
   // Adding event listeners to continents at start of game
@@ -123,6 +127,9 @@ var gameWinner = "";
 
 // Creating variable for winner of each turn
 var turnWinner = "";
+
+//creating variable for previous turn's player's success or failure
+var winFail = "";
 
 // Getting a random 1 or 0 to determine winner of each move.
 function randomWinner() {
@@ -185,26 +192,38 @@ function attackContinent() {
       this.style.backgroundColor = "#E52C1A";
       this.setAttribute('data-player', 'General');
       playerTurn = "Diplomat";
+      otherTurn = 'General';
+      winFail = ' took your continent away!'
     }
       // For General's turn, if turn not winner, click just changes turns to diplomat
       else if ((playerTurn === 'General') && (turnWinner !== 'General') && (this.getAttribute("data-player") == 'Diplomat')) {
       playerTurn = "Diplomat";
+      otherTurn = 'General';
+      winFail = ' failed to reduce your power!'
     }
     // For Diplomat's turn, if turn winner, attack on general contient changes it to Blue
       else if ((playerTurn === 'Diplomat') && (turnWinner === 'Diplomat') && (this.getAttribute("data-player") == 'General')) {
       this.style.backgroundColor = "#001775";
       this.setAttribute('data-player', 'Diplomat');
       playerTurn = "General";
+      otherTurn = 'Diplomat';
+      winFail = ' took your continent away!'
     }
       // For General's turn, if turn not winner, click just changes turns to general
       else if ((playerTurn === 'Diplomat') && (turnWinner !== 'Diplomat') && (this.getAttribute("data-player") == 'General')) {
       playerTurn = "General";
+      otherTurn = 'Diplomat';
+      winFail = ' failed to reduce your power!'
     }
   };
+
+  // Calling the function to check for a winner
+  checkWinner();
 
   // Creating new message for turn
   document.getElementById("turn-message").innerHTML = `${playerTurn}, it's your turn.`;
 
-  // Calling the function to check for a winner
-  checkWinner();
+  // Updating the status message
+  document.getElementById("status-text").innerHTML = `The ${otherTurn} ${winFail}`;
+
 };
